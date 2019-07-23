4f976dd0bd0eff2a287447d3dea8db9b88c5e7cf
Øyvind Follan <ofollan@gmail.com>
2019-07-23 08:39:23 +0200

    Legg til imports i index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import './main.css';
+// Oppgave 1
 import { Elm } from './Oppgave1/Main.elm';
+
+//Oppgave 2
+// import { Elm } from './Oppgave2/Fireworks.elm';
+
+//Ekstraoppgave
+// import { Elm } from './Ekstraoppgave/Main.elm';
+
 import registerServiceWorker from './registerServiceWorker';
 
 Elm.Main.init({
